b57334be4805891c842d1145e9f99073906ff4f3
Daniel Tweedie <djtwd.ix@icloud.com>
2021-01-07 17:55:59 +0000

    Added more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,5 @@
+// use conditionals to tell people about the weather
+
 const temperature = 3;
 
 if (temperature < 0) {
@@ -9,3 +11,24 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+// determine voting eligibility by age
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+// determine extreme weather
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+// determine umbrella necessity based on weather conditions
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
